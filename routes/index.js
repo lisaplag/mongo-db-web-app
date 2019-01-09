efa637b7305a25d13cb3ef2ef9476a0820d53527
Lisa <l.l.m.plag@student.tudelft.nl>
2019-01-09 09:59:58 +0100

    Added bin

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.status(200).json({ message: 'Connected!' });
-    res.render('../views/index');
-    console.log('A User connected to the home page.');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
